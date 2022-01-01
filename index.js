a150b4243d4bece27d5185ae89266ea3152ed1ac
Michael <browndifalcom@gmail.com>
2022-01-01 16:13:02 -0500

    commit tree d878840b0a57b0bfd0ef4f5f39851c8dbdbf4919

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function(divide){
+    return 2000/100
+}
+
+const square = x => x ** 2; 
+
+const add = (parameter1, parameter2) => {
+    console.log(`Adding ${parameter1}`);
+    console.log(`Adding ${parameter2}`);
+    return parameter1 + parameter2;
+  }
